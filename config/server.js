211daffe58d0d8752be78d30cc4a4b1c19211869
daooda <davinhoba9@outlook.fr>
2020-07-23 16:35:27 +0000

    first commit w/ Strapi 🎉

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bef4bc8369cd25c93706656fa16bc376'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ac081ac9-19bc-4363-ab70-9be15b199027'
+};
